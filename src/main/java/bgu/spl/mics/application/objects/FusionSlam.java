b11f08ecd59eeb2671215019e9ee57e7dc4ba018
Yoav Levin <yoavlev555@gmail.com>
2024-12-28 14:10:19 +0000

    implement objects

--- src/main/java/bgu/spl/mics/application/objects/FusionSlam.java
+++ src/main/java/bgu/spl/mics/application/objects/FusionSlam.java
@@ -1,13 +1,35 @@
 package bgu.spl.mics.application.objects;
-
+import java.util.LinkedList;
+import java.util.List;
 /**
  * Manages the fusion of sensor data for simultaneous localization and mapping (SLAM).
  * Combines data from multiple sensors (e.g., LiDAR, camera) to build and update a global map.
  * Implements the Singleton pattern to ensure a single instance of FusionSlam exists.
  */
 public class FusionSlam {
+    
     // Singleton instance holder
     private static class FusionSlamHolder {
-        // TODO: Implement singleton instance logic.
+        static final FusionSlam fs = new FusionSlam();  
+    }
+
+    private final List<LandMark> landmarks;
+    private final List<Pose> poses;
+
+    private FusionSlam(){
+        this.landmarks = new LinkedList<LandMark>();
+        this.poses = new LinkedList<Pose>();
+    }
+
+    public FusionSlam getInstance(){
+        return FusionSlamHolder.fs;
+    }
+
+    public List<LandMark> getLandmarks() {
+        return getInstance().landmarks;
+    }
+
+    public List<Pose> getPoses() {
+        return getInstance().poses;
     }
 }
